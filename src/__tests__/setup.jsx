00faa13b587265d29d4714910229f55ed55182dd
CindyAud <cindyaudrey3@gmail.com>
2026-06-07 12:59:33 +0300

    Fix setup.jsx: define setFetchResponse and fetch mock as globals

--- src/__tests__/setup.jsx
+++ src/__tests__/setup.jsx
@@ -4,3 +4,32 @@ import '@testing-library/jest-dom'
 afterEach(() => {
   cleanup()
 })
+
+global.basePlants = [
+  { id: 1, name: "Aloe", image: "./images/aloe.jpg", price: 15.99 },
+  { id: 2, name: "ZZ Plant", image: "./images/zz-plant.jpg", price: 25.98 },
+]
+
+global.alternatePlants = [
+  { id: 3, name: "Monstera", image: "./images/monstera.jpg", price: 25.99 },
+]
+
+global.setFetchResponse = (plants = global.basePlants) => {
+  global.fetch = jest.fn((url, options) => {
+    if (!options || options.method !== "POST") {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(plants),
+      })
+    }
+    const body = JSON.parse(options.body)
+    return Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve({ id: Date.now(), ...body }),
+    })
+  })
+}
+
+beforeEach(() => {
+  global.setFetchResponse()
+})
